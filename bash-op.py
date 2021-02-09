70a98426596be4d7f3a114befc2a2b22a8a870c0
alawu <alawu@tesla.com>
2021-02-09 14:02:52 -0500

    feat: 2.0 api

--- bash-op.py
+++ bash-op.py
@@ -1,80 +1,57 @@
-#
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
+import json
 
-"""Example DAG demonstrating the usage of the BashOperator."""
-
-from datetime import timedelta
-
-from airflow import DAG
-from airflow.operators.bash import EXIT_CODE_SKIP, BashOperator
-from airflow.operators.dummy import DummyOperator
+from airflow.decorators import dag, task
 from airflow.utils.dates import days_ago
-
-args = {
+# These args will get passed on to each operator
+# You can override them on a per-task basis during operator initialization
+default_args = {
     'owner': 'airflow',
 }
+@dag(default_args=default_args, schedule_interval=None, start_date=days_ago(2), tags=['example'])
+def tutorial_taskflow_api_etl():
+    """
+    ### TaskFlow API Tutorial Documentation
+    This is a simple ETL data pipeline example which demonstrates the use of
+    the TaskFlow API using three simple tasks for Extract, Transform, and Load.
+    Documentation that goes along with the Airflow TaskFlow API tutorial is
+    located
+    [here](https://airflow.apache.org/docs/stable/tutorial_taskflow_api.html)
+    """
+    @task()
+    def extract():
+        """
+        #### Extract task
+        A simple Extract task to get data ready for the rest of the data
+        pipeline. In this case, getting data is simulated by reading from a
+        hardcoded JSON string.
+        """
+        data_string = '{"1001": 301.27, "1002": 433.21, "1003": 502.22}'
 
-with DAG(
-    dag_id='example_bash_operator',
-    default_args=args,
-    schedule_interval='0 0 * * *',
-    start_date=days_ago(2),
-    dagrun_timeout=timedelta(minutes=60),
-    tags=['example', 'example2'],
-    params={"example_key": "example_value"},
-) as dag:
+        order_data_dict = json.loads(data_string)
+        return order_data_dict
+    @task(multiple_outputs=True)
+    def transform(order_data_dict: dict):
+        """
+        #### Transform task
+        A simple Transform task which takes in the collection of order data and
+        computes the total order value.
+        """
+        total_order_value = 0
 
-    run_this_last = DummyOperator(
-        task_id='run_this_last',
-    )
+        for value in order_data_dict.values():
+            total_order_value += value
 
-    # [START howto_operator_bash]
-    run_this = BashOperator(
-        task_id='run_after_loop',
-        bash_command='echo 1',
-    )
-    # [END howto_operator_bash]
+        return {"total_order_value": total_order_value}
+    @task()
+    def load(total_order_value: float):
+        """
+        #### Load task
+        A simple Load task which takes in the result of the Transform task and
+        instead of saving it to end user review, just prints it out.
+        """
 
-    run_this >> run_this_last
-
-    for i in range(3):
-        task = BashOperator(
-            task_id='runme_' + str(i),
-            bash_command='echo "{{ task_instance_key_str }}" && sleep 1',
-        )
-        task >> run_this
-
-    # [START howto_operator_bash_template]
-    also_run_this = BashOperator(
-        task_id='also_run_this',
-        bash_command='echo "run_id={{ run_id }} | dag_run={{ dag_run }}"',
-    )
-    # [END howto_operator_bash_template]
-    also_run_this >> run_this_last
-
-# [START howto_operator_bash_skip]
-this_will_skip = BashOperator(
-    task_id='this_will_skip',
-    bash_command=f'echo "hello world"; exit {EXIT_CODE_SKIP};',
-    dag=dag,
-)
-# [END howto_operator_bash_skip]
-this_will_skip >> run_this_last
-
-if __name__ == "__main__":
-    dag.cli()
+        print("Total order value is: %.2f" % total_order_value)
+    order_data = extract()
+    order_summary = transform(order_data)
+    load(order_summary["total_order_value"])
+tutorial_etl_dag = tutorial_taskflow_api_etl()
